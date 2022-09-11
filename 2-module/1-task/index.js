c7d0139bb7ab999dd638f52338516ab9031f5571
Vladislav <vladkovalev110@gmail.com>
2022-09-11 21:54:42 +0300

    feat: logic fr sumSalary func

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,21 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let newSalariesObj = [];
+  let sumSalaries = 0;
+
+  for (key in salaries) {
+    if (typeof (salaries[key]) === "number") {
+      if (!isNaN(salaries[key]) && salaries[key] != Infinity && salaries[key] != -Infinity) {
+        newSalariesObj.push(salaries[key]);
+      }
+    }
+  }
+
+  if (newSalariesObj.length === 0) {
+    return 0;
+  } else {
+    for (let i = 0; i < newSalariesObj.length; i++) {
+      sumSalaries += newSalariesObj[i];
+    }
+    return sumSalaries;
+  }
 }
