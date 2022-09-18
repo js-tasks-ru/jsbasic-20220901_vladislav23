384177d036e9580d0bfbdff10db950d08f25c913
Vladislav <vladkovalev110@gmail.com>
2022-09-19 00:51:30 +0300

    feat: logic for showSalary func

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,16 @@
 function showSalary(users, age) {
-  // ваш код...
+  let resultStr = "";
+
+  let filterArray = users.filter((item) => item.age <= age);
+  let nameArray = filterArray.map((item) => item.name);
+  let balanceArray = filterArray.map((item) => item.balance);
+  
+  for (let i = 0; i < nameArray.length; i++) {
+    resultStr +=
+      i === nameArray.length - 1
+        ? nameArray[i] + ", " + balanceArray[i]
+        : nameArray[i] + ", " + balanceArray[i] + "\n";
+  }
+
+  return resultStr;
 }
