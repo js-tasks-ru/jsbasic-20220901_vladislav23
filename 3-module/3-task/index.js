79f7da928d71006107889deb827e1e75b9895ea5
Vladislav <vladkovalev110@gmail.com>
2022-09-18 22:12:28 +0300

    feat: logic for camelize func

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,12 @@
 function camelize(str) {
-  // ваш код...
+  let splitArray = str.split('-');
+  let ucFirstArray = [];
+
+  for (let i = 1; i < splitArray.length; i++) {
+    let word = splitArray[i];
+
+    ucFirstArray.push(word[0].toUpperCase() + word.slice(1, word.length));
+  }
+
+  return splitArray[0] + ucFirstArray.join("");
 }
