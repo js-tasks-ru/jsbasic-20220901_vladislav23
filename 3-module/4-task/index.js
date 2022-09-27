42a427c30ef972f37882874f48f484d4b7522171
Vladislav <vladkovalev110@gmail.com>
2022-09-28 00:55:31 +0300

    Review fixes

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,16 +1,6 @@
 function showSalary(users, age) {
-  let resultStr = "";
-
-  let filterArray = users.filter((item) => item.age <= age);
-  let nameArray = filterArray.map((item) => item.name);
-  let balanceArray = filterArray.map((item) => item.balance);
-  
-  for (let i = 0; i < nameArray.length; i++) {
-    resultStr +=
-      i === nameArray.length - 1
-        ? nameArray[i] + ", " + balanceArray[i]
-        : nameArray[i] + ", " + balanceArray[i] + "\n";
-  }
-
-  return resultStr;
+  return users
+    .filter((item) => item.age <= age)
+    .map((item) => item.name + ", " + item.balance)
+    .join("\n");
 }
